10cf108b5f1881befa67510d0347269fd9711917
leosole <8992517+leosole@users.noreply.github.com>
2021-12-09 20:01:41 -0300

    detecção de interseção das linhas + memória

---
+++ mylib.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+from collections.abc import Iterable
+
+
+#Aplica uma máscara é obtem a ROI
+def interested_region(img, vertices):
+    #if len(img.shape) > 2: 
+    #    mask_color_ignore = (255,) * img.shape[2]
+    #else:
+    #    mask_color_ignore = 255
+    #    
+    #cv2.fillPoly(np.zeros_like(img), vertices, mask_color_ignore)
+    #return cv2.bitwise_and(img, np.zeros_like(img))
+    mask = np.zeros_like(img)    
+    mask = cv2.fillPoly(mask, vertices, 255)
+    mask = cv2.bitwise_and(img, mask)
+    return mask
+
+# Faz um Canny automático
+def auto_canny(image, sigma=0.33):
+    v = np.median(image)
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    return edged
+
+# Faz a média das linhas da esquerda e da direita (através do coeficiente angular).
+def average(image, lines, lane_height, prev_lines):
+	left = []
+	right = []
+	for line in lines:
+		x1, y1, x2, y2 = line.reshape(4)
+		parameters = np.polyfit((x1, x2), (y1, y2), 1)
+		slope = parameters[0]
+		y_intercept = parameters[1]
+
+		# Normalmente um slope positivo = left line  e slope negativo = right line mas neste caso, 
+		# o eixo Y da imagem é invertido, logo os slopes são invertidos (OpenCV tem eixo Y invertido).
+		if slope < 0:
+			left.append((slope, y_intercept))
+		else:
+			right.append((slope, y_intercept))
+
+	# Pega a média das linhas
+	right_avg = np.average(right, axis=0)
+	left_avg = np.average(left, axis=0)
+
+	# Se não houver linhas, usar linhas anteriores
+	if prev_lines is not None:
+		if np.isnan(right_avg).any() and (prev_lines[1] != [0,0,0,0]).all():
+			prev_right = prev_lines[1]
+			prev_right_parameters = np.polyfit((prev_right[0], prev_right[2]), (prev_right[1], prev_right[3]), 1)
+			right_avg = prev_right_parameters
+		if np.isnan(left_avg).any() and (prev_lines[0] != [0,0,0,0]).all():
+			prev_left = prev_lines[0]
+			prev_left_parameters = np.polyfit((prev_left[0], prev_left[2]), (prev_left[1], prev_left[3]), 1)
+			left_avg = prev_left_parameters
+
+	# Calcula interseção das linhas
+	# x_i (b1-b2) / (m2-m1)
+	if not np.isnan(right_avg).any() and not np.isnan(left_avg).any():
+		x_i = (right_avg[1]-left_avg[1]) / (left_avg[0]-right_avg[0])
+		lane_height = (right_avg[0] * x_i + right_avg[1]) / image.shape[0] + 0.02
+
+	# Pega os pontos de início e fim de cada linha
+	left_line = make_points(image, left_avg, lane_height)
+	right_line = make_points(image, right_avg, lane_height)
+
+	return np.array([left_line, right_line])
+
+# Cria os pontos das linhas finais
+def make_points(image, average, lane_height=0.8): 
+	theRet = np.array([0,0,0,0])
+
+	if isinstance(average, Iterable):
+		slope, y_int = average 
+		y1 = image.shape[0]
+
+		# Define a altura das linhas que será a mesma para ambas
+		y2 = int(y1 * (lane_height))  # Constante lane_height bastante dependente da imagem
+
+		#Se y=mx+b   então   x = (y-b) / m
+		x1 = int((y1 - y_int) // slope)
+		x2 = int((y2 - y_int) // slope)
+
+		theRet = np.array([x1, y1, x2, y2])
+
+	return theRet
+
+# Cria camada com as linhas
+def display_lines(image, lines):
+	lines_image = np.zeros_like(image)
+	if lines is not None:
+		for line in lines:
+			x1, y1, x2, y2 = line
+			cv2.line(lines_image, (x1, y1), (x2, y2), (0, 0, 255), 10)
+	return lines_image
---
+++ run.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+import time
+import imutils
+import glob
+from configparser import ConfigParser
+import matplotlib.pyplot as plt
+import mylib as mylib
+
+#Leitura das configurações
+config_object = ConfigParser(allow_no_value=True)
+config_object.read("config.ini")
+param = config_object["PARAMETERS"]
+inputdata = config_object["INPUT"]
+
+#Definições de constantes
+DEBUG = bool(int(param['DEBUG'])) 
+SOURCE = inputdata['VIDEO'] 
+LANE_HEIGHT = float(param['LANE_HEIGHT']) 
+
+#Inicializações
+debugStart = 0
+debugStop = 0
+debugCtd = 1
+
+# Abre o vídeo
+cap = cv2.VideoCapture(SOURCE)
+
+# Verifica se a captura de vídeo teve sucesso
+if (cap.isOpened()== False): 
+	print("ERRO: Não foi possível abrir a entrada de vídeo")
+
+# Inicializa o cronômetro para o cálculo de fps
+if(DEBUG):
+	debugStart = time.time()
+
+# Fica lendo o vídeo
+while(cap.isOpened()):
+	# Conta os frames para realizar o cálculo de fps (estou aceitando que inicialize com uma unidade a mais)
+	if(DEBUG):
+		debugCtd = debugCtd + 1
+
+	# Captura frame-por-frame
+	ret, frame = cap.read()
+	if ret == True:
+		#Reduz o tamanho da imagem
+		#frameResized = imutils.resize(frame, width=IMAGE_SIZE)
+		imageHeight = frame.shape[0]
+		imageWidth = frame.shape[1]
+
+		# Pré-processamento
+		frameView = frame.copy()
+
+		frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+		frameBlur= cv2.GaussianBlur(frameGray, (5, 5), 0)
+		
+		# Dependente de dataset: as linhas da estrada são sempre amarelas e brancas. O Amarelo é um 
+		# transtorno para ser isolado no espaço RBG, então converte para o espaço HSV (Hue Value Saturation)
+		# A escolha abaixo de amarelo têm sido muito usada nos tutoriais, mas pode ser modificada. 
+		# Para a linha branca, trabalho com os níveis de conza mesmo.
+		lower_yellow = np.array([20, 100, 100], dtype = "uint8")
+		upper_yellow = np.array([30, 255, 255], dtype="uint8")
+		mask_yellow = cv2.inRange(frameHSV, lower_yellow, upper_yellow)
+		mask_white = cv2.inRange(frameGray, 200, 255)
+		mask_yw = cv2.bitwise_or(mask_white, mask_yellow)
+		frameYWMask = cv2.bitwise_and(frameGray, mask_yw)
+
+		# Tavez devesse tentar pegar a faixa amarela no Canny passando frameYWMask, por hora, não é usado.
+		frameCanny = mylib.auto_canny(frameBlur)
+
+		# Dependente de dataset: Aplica uma máscara trapezoidal no objeto de interesse.
+		imshape = frameGray.shape
+		# Opção original
+		lower_left = [imshape[1]/9,imshape[0]]
+		lower_right = [imshape[1]-imshape[1]/9,imshape[0]]
+		top_left = [imshape[1]/2-imshape[1]/8,imshape[0]/2+imshape[0]/10]
+		top_right = [imshape[1]/2+imshape[1]/8,imshape[0]/2+imshape[0]/10]
+		# Opção baseada no minha visão já com viés
+		lower_left =  [  imshape[1]/9 ,   imshape[0]    ]
+		lower_right = [7*imshape[1]/9 ,   imshape[0]    ]
+		top_left =    [4*imshape[1]/9 , 7*imshape[0]/10 ]
+		top_right =   [4*imshape[1]/9 , 7*imshape[0]/10 ]
+		vertices = [np.array([lower_left,top_left,top_right,lower_right],dtype=np.int32)]
+		frameROI = mylib.interested_region(frameCanny, vertices)
+
+		# Transformada Hough
+		# Param 1: gradientes isolados
+		# Param 2 e 3: Definindo o tamanhol do bin, 2 é e 3 é theta
+		# Param 4: Mínima quantidade de interseções necessárias para um bin ser considerado linha 
+		# Param 5: Placeholder array
+		# Param 6: Comprimento mínimo da linha
+		# Param 7: Máximo de falhas na linha
+		lines = cv2.HoughLinesP(frameROI, rho=2, theta=np.pi/180, threshold=80, 
+			lines=np.array([]), minLineLength=40, maxLineGap=50)
+
+		if lines is None:
+			print("ERRO: Nenhuma linha encontrada")
+			break
+		
+		# Faz a média das linhas da tranformada 
+		if 'left_and_right_lines' in locals():
+			left_and_right_lines = mylib.average(frameROI,lines,LANE_HEIGHT, left_and_right_lines)
+		else:
+			left_and_right_lines = mylib.average(frameROI,lines,LANE_HEIGHT, None)
+
+		# Prepara para fazer display
+		black_lines = mylib.display_lines(frameView, left_and_right_lines)
+		frameLanes = cv2.addWeighted(frameView, 0.8, black_lines, 1, 1)
+	
+		# Faz o display em uma janela
+		if(DEBUG):
+			d3_frameGray = cv2.cvtColor(frameGray, cv2.COLOR_GRAY2BGR)
+			d3_frameBlur = cv2.cvtColor(frameBlur, cv2.COLOR_GRAY2BGR)			
+			d3_frameCanny = cv2.cvtColor(frameCanny, cv2.COLOR_GRAY2BGR)
+			d3_frameCanny = cv2.polylines(d3_frameCanny, vertices, True ,(0,0,255),2)
+			d3_frameYWMask = cv2.cvtColor(frameYWMask, cv2.COLOR_GRAY2BGR)			
+			d3_frameROI = cv2.cvtColor(frameROI, cv2.COLOR_GRAY2BGR)
+		
+			numpy_horizontal1 = np.hstack((frameLanes, d3_frameCanny))
+			numpy_horizontal2 = np.hstack((d3_frameYWMask, d3_frameROI))
+
+			numpy_2x2 = np.vstack((numpy_horizontal1, numpy_horizontal2))
+			cv2.imshow('Frame',numpy_2x2)
+
+
+		# Pressiona tecla Q para sair
+		if cv2.waitKey(25) & 0xFF == ord('q'):
+			break
+
+	# Finaliza o loop se não conseguir sucesso ao ler o video de entrada
+	else: 
+		print("ERRO: Não foi possível ler a entrada de vídeo")
+		break
+
+# Finaliza o cronômetro e cálcula o fps
+if(DEBUG):
+	debugStop = time.time()
+	fps = debugCtd/(debugStop-debugStart)
+	print("DEBUG: {} fps\n".format(fps))
+
+# Libera o video capture object
+cap.release()
+
+# Fecha todas as janelas
+cv2.destroyAllWindows()
+
+
+
+
+
